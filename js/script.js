754ddb4bd029201f04bb76f287b9fd284f3e8aea
Bailey Frye <bailey@Baileys-MacBook-Pro.local>
2024-03-25 13:49:28 -0500

    Adding modal functionality

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import workWithUs from './components/workWithUs.js';
 import formVal from './components/formVal.js';
 
 const contactForm = document.querySelector('.contact-form');
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 document.addEventListener('readystatechange', (event) => {
   if (event.target.readyState === 'complete') {
@@ -15,3 +17,19 @@ document.addEventListener('readystatechange', (event) => {
 });
 
 if (contactForm) formVal();
+
+if (modal) {
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      modal.showModal();
+      modal.style.display = 'flex';
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target.classList.contains('close-modal')) {
+        modal.close();
+        modal.style.display = 'none';
+      }
+    });
+  });
+}
